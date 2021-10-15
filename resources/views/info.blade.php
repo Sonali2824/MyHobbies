8b34dc4e75c75593b66d93d20fe6d2029fa425fa
Sonali2824 <sovinara2824@gmail.com>
2021-10-15 13:28:09 +0530

    Updated

---
+++ resources/views/info.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('page_title',"About Hiver")
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Info</div>
+
+                <div class="card-body">
+                  Create accountability with email ownership and status
+Hiver makes sure every email coming to your customer service email address has a clear status. You can assign emails to team members, ensuring clear accountability and timely responses to your customers.
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Starting Page</div>
+
+                <div class="card-body">
+                  reate accountability with email ownership and status
+Hiver makes sure every email coming to your customer service email address has a clear status. You can assign emails to team members, ensuring clear accountability and timely responses to your customers.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
